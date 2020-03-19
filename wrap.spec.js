9ff42bf26fdf8133e5559321917ac70a13178481
Saj King <a.sajking@gmail.com>
2020-03-19 12:18:49 -0400

    first tests done

---
+++ wrap.spec.js
@@ -0,0 +1,39 @@
+const { expect } = require('chai');
+const wrap = require('./wrap');
+
+describe('wrap', ()=>{
+
+  const input1 = "bfkabdnfkabfkjabdfka"
+  const input2 = "dannna2ddhh dhhhja  shdkjahsdka jhsd k ajh dakj shdkajshd  i82"
+  const input3 = "22342"
+
+
+  it('returns an empty sting if input is empty', ()=>{
+    expect(wrap('')).to.equal('')
+  })
+  it('returns a string', ()=>{
+
+    expect(typeof wrap(input1).to.equal('string'))
+    expect(typeof wrap(input2).to.equal('string'))
+    expect(typeof wrap(input3).to.equal('string'))
+  })
+  it('returns lines =< 20 characters long', ()=>{
+    const textArray = wrap(input2).split('\n');
+
+    textArray.map((currentLine)=>{
+      expect(currentLine.length<=20).to.equal(true)
+    })
+  })
+  it("doesn't break words up between lines", ()=>{
+    expect(input2.split(' ').length).to.equal(wrap(input2).split(' ').length)
+  })
+})
+
+
+/*
+tests:
+-requires nonempty string
+-returns a string
+-line length < 20
+-doesn't split words between lines
+*/
